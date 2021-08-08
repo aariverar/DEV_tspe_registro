8752b033122e2cf6af2f2987a76b25e80dcb5265
Abraham Rivera <38504600+aariverar@users.noreply.github.com>
2021-08-07 20:34:14 -0500

    update
    change bd

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
-$conexion=mysqli_connect("localhost","root","","tspe_registros");
+$conexion=mysqli_connect("bfrjjmrh4mrn5h04ltgi-mysql.services.clever-cloud.com","upngpfhdqtbsetus","mrxBzwRgNSThtNva01Xp","bfrjjmrh4mrn5h04ltgi");
 ?>
