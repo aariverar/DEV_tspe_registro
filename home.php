d20aef5c8c1b97d747469bbb37e3052da51e99b7
aariverar <aalexriverar@gmail.com>
2021-08-08 03:10:34 -0500

    se modifica el menu con mas opciones

--- home.php
+++ home.php
@@ -14,11 +14,16 @@
             <label for="btn-menu"><img src="img/menu.png" alt=""></label>
             <nav class="menu">
                 <ul>
-                    <li><a href="">Inicio</a></li>
-                    <li><a href="">Registro Capacitaciones</a></li>
-                    <li><a href="calculadora/index.php">Calculadora de Estimación</a></li>
-                    <li><a href="">Reporte 1</a></li>
-                    <li><a href="">Reporte 2</a></li>
+                    <li><a href="https://ppm.tsoftglobal.com/itg/dashboard/app/portal/PageView.jsp" target="_blank">TSOFT: Registrar Horas PPM</a></li>
+                    <li><a href="https://ayudatsoft.atlassian.net/servicedesk/customer/user/login?destination=portals" target="_blank">TSOFT: Crear Tickets JSM</a></li>
+                    <li><a href="https://tsoft.hcwork.com/ingresar" target="_blank">TSOFT: HCWORK</a></li>
+                    <li><a href="calculadora/index.php">TSOFT: Calculadora de Estimación</a></li>
+                    <li><a href="">TSOFT: Registro Capacitaciones</a></li>
+                    <li><a href="https://tsoftglobal.atlassian.net/jira/software/c/projects/GDLP/issues" target="_blank" >La Positiva: Registro de Horas</a></li>
+                    <li><a href="https://trello.com/b/gJNruRku/despliegues-la-positiva" target="_blank">La Positiva: Trello Despligues</a></li>
+                    <li><a href="reportehoras/horas.php">La Positiva: Power BI Reporte Horas</a></li>
+                    <li><a href="https://tsoftlatam.sharepoint.com/teams/PEPRODLaPositivaRQ/Documentos%20compartidos/Forms/AllItems.aspx?RootFolder=%2Fteams%2FPEPRODLaPositivaRQ%2FDocumentos%20compartidos%2F02%20Manuales%20y%20Gu%C3%ADas&FolderCTID=0x012000435E17B335A54840BB3E938A1DF9F1D1" target="_blank">La Positiva: Gestión de Conocimiento</a></li>
+                    <li><a href="">Agrega...</a></li>
                     <li><a href="index.html">Salir</a></li>
                 </ul>
             </nav>
---
+++ reportehoras/horas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Reporte Horas La Positiva</title>
+		<link rel="icon" href="img/icono.png" />
+
+    </head>
+        <body>
+            
+            <h1 style="text-align:center; width:750px;height:0px; margin-left:auto; margin-right:auto; margin-bottom:1%; border:0px solid #000;font-size:30px; font-family:verdana;font-weight:bold">Reporte de Horas La Positiva</h1>
+            <br>
+            <br>
+            <br>
+            <form action="../home.php" method="post">
+            <p align="center"><iframe width="1280" height="650" src="https://app.powerbi.com/reportEmbed?reportId=c7a7adce-5480-4e36-87fe-e1ed06d69e41&autoAuth=true&ctid=ed811312-91e0-448f-9cc9-9e9e15b6b836&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWNlbnRyYWwtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="1" allowFullScreen="true"></iframe></p>
+            <br>
+            <br>
+            <p align="center"><button type="btn-volver" style="font-family:verdana;color:white;font-weight:bold;background-color: #c00000;width:10%; height:10%" href="../home.php">Volver</button></p>
+            </form>
+        </body>
+    
+</html>
